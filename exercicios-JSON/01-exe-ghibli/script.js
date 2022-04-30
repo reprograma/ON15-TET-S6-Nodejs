cf4e94c1cf94d2dc7741a8acc765a84816f04b64
amitairferreira <amitairlima@gmail.com>
2022-04-30 16:29:53 -0300

    Atualização do repositório pós aula

--- exercicios-JSON/01-exe-ghibli/script.js
+++ exercicios-JSON/01-exe-ghibli/script.js
@@ -27,7 +27,11 @@ const jsonDeFilmes = [
 // COMEÇA O EXERCÍCIO
 
 
-
+for(let i=0; i < jsonDeFilmes.length; i++ ){
+  let filme = jsonDeFilmes[i]
+  console.log(filme.title)
+  console.log(filme.description)
+}
   
 
   
