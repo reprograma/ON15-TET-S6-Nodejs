b5bfb81052a412add9859f487ecec18966b4a6c7
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2022-05-04 19:56:17 -0300

    exercicios da aula

--- exercicios-JSON/02-exe-pokemon/script.js
+++ exercicios-JSON/02-exe-pokemon/script.js
@@ -36,3 +36,19 @@ const data = [
 
 // COMEÇA O EXERCÍCIO
 
+// 14h13
+
+
+for(let i=0; i < data.length; i++){
+    let pokemon = data[i]
+    console.log(pokemon.name)
+    console.log(pokemon.description)
+    // console.log(pokemon.types)
+
+    let tipos = pokemon.types
+
+    for(let j=0; j< tipos.length; j++){
+        console.log(tipos[j])
+    }
+}
+
